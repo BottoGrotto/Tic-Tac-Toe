568cde0a75729c0ebd94881a9e157ea5372c5cd1
BottoGrotto <coolsoccerotto@gmail.com>
2024-02-21 14:05:27 -0800

    Third Commit - changed how different userInputs are handled

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,4 +1,4 @@
-import os, random
+import os, random, time
 
 grid: list = [[
                 "¹", "²", "³"
@@ -37,7 +37,7 @@ def printOutGrid(list: list) -> None:
             output += "\n"
     print(output)
 
-def updateGrid(userInput: int, userType: str) -> bool:
+def updateGrid(player: str, userInput: int, userType: str) -> bool:
     pos = 1
     for r, row in enumerate(grid):
         for c, col in enumerate(grid[r]):
@@ -46,13 +46,21 @@ def updateGrid(userInput: int, userType: str) -> bool:
                 pos += 1
                 if userInput == pos-1 and grid[r][c] != "X" and grid[r][c] != "O":
                     grid[r][c] = userType
-                    os.system("cls")
+                    os.system("clear")
                     return True
                 elif userInput == pos-1 and grid[r][c] == "X" and grid[r][c] == "O":
-                    os.system("cls")
+                    os.system("clear")
+                    printEssentials(player, grid)
+                    inputCheckerOut = BadInputChecker(player, userType)
+                    # stata = inputCheckerOut[0]
+                    userInput = inputCheckerOut[1]
+                    player = inputCheckerOut[2][0]
+                    userType = inputCheckerOut[2][1]
+                    
                     return False
                 else:
-                    os.system("cls") 
+                    os.system("clear") 
+    
 
 def findRemaining() -> list:
     listToReturn = []
@@ -78,7 +86,8 @@ def printEssentials(player, grid) -> None:
     printPlayer(player)
     printOutGrid(grid)
 
-def updateTurns(stata, player, user1Name, user2Name, userType, user1Type, user2Type) -> None:
+def updateTurns(stata: bool, player: str, user1Name: str, user2Name: str, userType: str, user1Type: str, user2Type: str) -> list:
+    """Switches who the player that is going to be displayed and their type based on who just went"""
     if stata == True:
         if player == user1Name:
             player = user2Name
@@ -90,6 +99,41 @@ def updateTurns(stata, player, user1Name, user2Name, userType, user1Type, user2T
             return [player, userType]
     else:
         return [player, userType]
+    
+
+def updateTheData(player: str, user1Name: str, user2Name: str, userType: str, user1Type: str, user2Type: str, userInput: int) -> list:
+    stata = updateGrid(player, userInput=userInput, userType=userType)
+    return updateTurns(stata, player, user1Name, user2Name, userType, user1Type, user2Type)
+
+def BadInputChecker(player: str, userType: str) -> list:
+    """Tells user to input a number of one of the remaining slots"""
+    theChecker = True
+    while theChecker == True:
+        os.system("clear")
+        printEssentials(player, grid)
+        userInput = input(f"Please input any of these numbers:| {findRemaining()} |: ")
+        if str(userInput).isnumeric():
+            # print("Woah now")
+            if checkUserInput(int(userInput)) == True:
+                # print("Print this")
+                # time.sleep(3)
+                stata = updateGrid(player, userInput, userType)
+                playerUserType = updateTheData(player, user1Name, user2Name, userType, user1Type, user2Type, int(userInput))
+                theChecker = False
+        # else:
+        #     while userInput.isnumeric() == False:
+        #         userInput = input("Please input a real number!: ")
+                # if userInput.isnumeric() == True:
+                #     print("This is a test")
+                #     time.sleep(4)
+                #     playerUserType = updateTheData(player, user1Name, user2Name, userType, user1Type, user2Type, int(userInput))
+                   
+                    
+                    # player = playerUserType[0]
+                    # userType = playerUserType[1]
+    # os.system("clear")
+    
+    return [stata, userInput, playerUserType]
 #  1 1 1
 #  = = =
 #  1 1 1
@@ -114,7 +158,7 @@ gameState = 1
 player = ""
 winnerType = "X"
 stata = True
-os.system("cls")
+os.system("clear")
 if __name__ == "__main__":
     while userInput != "stop":
         if gameState == 1:
@@ -132,50 +176,46 @@ if __name__ == "__main__":
                 user2Type = "X"
                 user1Type = "O"
 
-            os.system("cls")
+            os.system("clear")
             gameState = 2
-
+        
         if gameState == 2:
-            os.system("cls")
+            os.system("clear")
             printEssentials(player, grid)
-            userInput = input("Choose place to switch: ")
+            userInput = input(f"Input any of these numbers:| {findRemaining()} |: ")
             if userInput == "stop":
                 break
 
             didSomeonePutAStr = userInput.isnumeric()
             while didSomeonePutAStr != True:
-                userInput = input("Please input a number!: ")
-                didSomeonePutAStr = userInput.isnumeric()
-                stata = updateGrid(userInput=int(userInput), userType=userType)
-                os.system("cls")
-                playerUserType = updateTurns(stata=stata, player=player,user1Name=user1Name, user2Name=user2Name, userType=userType, user1Type=user1Type, user2Type=user2Type)
-                player = playerUserType[0]
-                userType = playerUserType[1]
+                os.system("clear")
                 printEssentials(player, grid)
+                print("WOOOO")
+                time.sleep(2)
+                userInput = input(f"Please input any of these numbers:| {findRemaining()} |: ")
+                didSomeonePutAStr = userInput.isnumeric()
+                if didSomeonePutAStr:
+                    stata = updateGrid(player, userInput=int(userInput), userType=userType)
+                os.system("clear")
+                inputCheckerOut = BadInputChecker(player, userType)
+                stata = inputCheckerOut[0]
+                userInput = inputCheckerOut[1]
+                player = inputCheckerOut[2][0]
+                userType = inputCheckerOut[2][1]
             
             didSomeonePutAStr = userInput.isnumeric()
             while didSomeonePutAStr:
                 userInput = int(userInput)
-                stata = updateGrid(userInput, userType=userType)
+                stata = updateGrid(player, userInput, userType=userType)
                 if (userInput > 9):
-                    theChecker = checkUserInput(int(userInput))
-                    while theChecker == True:
-                        printEssentials(player, grid)
-                        userInput = input(f"Please input any of these numbers:| {findRemaining()} |: ")
-                        if str(userInput).isnumeric():
-                            stata = updateGrid(userInput=userInput, userType=userType)
-                            theChecker = False
-                        else:
-                            while userInput.isnumeric() == False:
-                                userInput = input("Please input a number!: ")
-                            if userInput.isnumeric() == True:
-                                stata = updateGrid(userInput=int(userInput), userType=userType)
-                                playerUserType = updateTurns(stata=stata, player=player,user1Name=user1Name, user2Name=user2Name, userType=userType, user1Type=user1Type, user2Type=user2Type)
-                                player = playerUserType[0]
-                                userType = playerUserType[1]
+                    inputCheckerOut = BadInputChecker(player, userType)
+                    stata = inputCheckerOut[0]
+                    userInput = inputCheckerOut[1]
+                    player = inputCheckerOut[2][0]
+                    userType = inputCheckerOut[2][1]          
                 else:
                     didSomeonePutAStr = False
-            
+
             playerUserType = updateTurns(stata=stata, player=player,user1Name=user1Name, user2Name=user2Name, userType=userType, user1Type=user1Type, user2Type=user2Type)
             player = playerUserType[0]
             userType = playerUserType[1]
